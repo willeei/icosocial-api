84a79db061956805cfd2ae57a9629b17bdafc954
Williams Silva <williams.silva@linx.com.br>
2020-04-16 11:57:03 -0300

    Removing old password verification

--- src/app/controllers/NewPasswordController.js
+++ src/app/controllers/NewPasswordController.js
@@ -4,15 +4,11 @@ import User from '../models/User';
 
 class NewPasswordController {
   async update(req, res) {
-    const { oldPassword, password } = req.body;
+    const { password } = req.body;
     const user = await User.findOne({ _id: req.user.id });
 
     if (!user) {
       return res.status(404).json({ error: 'User not exists' });
-    }
-
-    if (oldPassword && !(await user.comparePassword(oldPassword))) {
-      return res.status(401).json({ error: 'Password does not match' });
     }
 
     const passwd = await bcrypt.hashSync(password, 8);
