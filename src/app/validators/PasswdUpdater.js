7a1cd6ee376970d6aeb998f71f7eb6354c9ae093
Williams Silva <williams.silva@linx.com.br>
2020-04-16 12:00:08 -0300

    New route
    - Add new password update validation.
    - Change in the recoverPasswd route to be called / passwd / recover /
    - Addition of the / passwd / update route used after password recovery

---
+++ src/app/validators/PasswdUpdater.js
@@ -0,0 +1,22 @@
+import * as Yup from 'yup';
+
+export default async (req, res, next) => {
+  try {
+    const schema = Yup.object().shape({
+      password: Yup.string()
+        .required()
+        .min(6),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    await schema.validate(req.body, { abortEarly: false });
+
+    return next();
+  } catch (err) {
+    return res
+      .status(400)
+      .json({ error: 'Validation fails', messages: err.inner });
+  }
+};
--- src/routes.js
+++ src/routes.js
@@ -7,12 +7,14 @@ import UserController from './app/controllers/UserController';
 import AuthController from './app/controllers/AuthController';
 import FileController from './app/controllers/FileController';
 import DonorController from './app/controllers/DonorController';
+import NewPasswordController from './app/controllers/NewPasswordController';
 import PasswordForgottenController from './app/controllers/PasswordForgottenController';
 import ActiveOrDesableAccountController from './app/controllers/ActiveOrDesableAccountController';
 
 import validateAuthStore from './app/validators/AuthStore';
 import validateUserStore from './app/validators/UserStore';
 import validateUserUpdater from './app/validators/UserUpdater';
+import validatePasswdUpdater from './app/validators/PasswdUpdater';
 
 import authMiddleware from './app/middlewares/auth';
 
@@ -20,7 +22,7 @@ const routes = Router();
 const upload = multer(multerConfig);
 
 routes.post('/users', validateUserStore, UserController.store);
-routes.put('/users/recoverPasswd', PasswordForgottenController.update);
+routes.put('/users/passwd/recover', PasswordForgottenController.update);
 
 routes.post('/auth', validateAuthStore, AuthController.store);
 
@@ -29,6 +31,11 @@ routes.post('/donors/anonymous', DonorController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', validateUserUpdater, UserController.update);
+routes.put(
+  '/users/passwd/update',
+  validatePasswdUpdater,
+  NewPasswordController.update
+);
 
 routes.put('/account/active', ActiveOrDesableAccountController.store);
 routes.delete('/account/disable', ActiveOrDesableAccountController.destroy);
